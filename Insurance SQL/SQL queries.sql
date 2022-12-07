84306b1faeb859505e443a52f70152e7b4615e55
abhipatil119 <patil.abhis@northeastern.edu>
2022-12-07 02:00:11 -0500

    Cust_reg back end coding

--- Insurance SQL/SQL queries.sql
+++ Insurance SQL/SQL queries.sql
@@ -3,8 +3,38 @@ use insurance;
 
 ----- create and Validation Login Page ----------------
 -- CREATE TABLE ValidationLogin(loginas varchar(30), companyname varchar(30), username varchar(30), password varchar(30));
-insert into ValidationLogin values("customer","", "sagar", "renuke");
+-- insert into ValidationLogin values("salesagency","", "kartik", "dixit");
 -- drop table ValidationLogin;
 -- select * from ValidationLogin;
-SELECT  loginas, username, password, companyname FROM ValidationLogin
+-- SELECT  loginas, username, password, companyname FROM ValidationLogin
 -- SELECT  loginas, username, password FROM ValidationLogin
+
+
+-- ----------------  CREATING_TABLES---------
+-- create table HealthClaim(
+-- custID int,
+-- gender varchar(8),
+-- DOB varchar(70),
+-- tobacco_user varchar(5),
+-- pregnant_adopting varchar(5),
+-- chronic_condition varchar(150),
+-- expected_income int,
+-- height float,
+-- weight float,
+-- premium int,
+-- salesbroker_id int
+-- );
+-- --------------------------
+
+-- create table PetClaim(
+-- custID int,
+-- petname varchar(30),
+-- species varchar(5),
+-- sex varchar(6),
+-- breed varchar(40),
+-- age varchar(20),
+-- zip_code int,
+-- premium int,
+-- salesbroker_id int
+-- );
+-- --------------------------
