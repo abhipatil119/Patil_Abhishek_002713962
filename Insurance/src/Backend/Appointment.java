3b6fc55927edf102c7a1fe4025086e3098a810bc
abhipatil119 <patil.abhis@northeastern.edu>
2022-12-07 07:22:35 -0500

    appointment booking class

---
+++ Insurance/src/Backend/Appointment.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Backend;
+
+/**
+ *
+ * @author patil
+ */
+public class Appointment {
+    String company;
+    String cust_id;
+    String r;
+    String date_of_app;
+    String time;
+}
