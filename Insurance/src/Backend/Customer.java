36dc1028bb13ac758d22620e39e68cca83bdc422
abhipatil119 <patil.abhis@northeastern.edu>
2022-12-08 03:50:38 -0500

    finance starting

---
+++ Insurance/src/Backend/Customer.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Backend;
+
+/**
+ *
+ * @author patil
+ */
+public class Customer {
+    String cust_id ; 
+
+    public Customer(String cust_id) {
+        this.cust_id = cust_id;
+    }
+
+    public String getCust_id() {
+        return cust_id;
+    }
+
+    public void setCust_id(String cust_id) {
+        this.cust_id = cust_id;
+    }
+}
